8038901bc99c2d6571a7d5271364f7d8071a18db
Carl Lagerstedt <105862361+Carl595@users.noreply.github.com>
2023-11-28 11:57:20 +0100

    Remove unnecessary imports

--- PointOfSaleSystem/PointOfSaleSystem/MainWindow.xaml.cs
+++ PointOfSaleSystem/PointOfSaleSystem/MainWindow.xaml.cs
@@ -9,8 +9,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System;
-using System.IO;
 
 namespace PointOfSaleSystem
 {
