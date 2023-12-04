a5a435b22250ae396b93d241cc2123b5cfd4c39c
Carl595 <105862361+Carl595@users.noreply.github.com>
2023-12-04 09:18:13 +0100

    Fix test for buttons from json
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>

--- PointOfSaleSystem/TestSystem/UnitTest1.cs
+++ PointOfSaleSystem/TestSystem/UnitTest1.cs
@@ -33,7 +33,7 @@ namespace TestSystem
         [TestMethod]
         public void TestPriceCalculation()
         {
-            Button button = window.FindFirstDescendant(cf.ByAutomationId("addProductButton")).AsButton();
+            Button button = window.FindFirstDescendant(cf.ByName("Kaffe")).AsButton();
             Label totalPrice = window.FindFirstDescendant(cf.ByAutomationId("totalPrice")).AsLabel();
 
             Trace.Assert(totalPrice.Text == "0,00 kr");
@@ -50,7 +50,7 @@ namespace TestSystem
         [TestMethod]
         public void TestResetPrice()
         {
-            Button button = window.FindFirstDescendant(cf.ByAutomationId("addProductButton")).AsButton();
+            Button button = window.FindFirstDescendant(cf.ByName("Kaffe")).AsButton();
             Button buttonReset = window.FindFirstDescendant(cf.ByAutomationId("resetButton")).AsButton();
             Label totalPrice = window.FindFirstDescendant(cf.ByAutomationId("totalPrice")).AsLabel();
 
@@ -74,27 +74,6 @@ namespace TestSystem
 
             Label totalPrice = window.FindFirstDescendant(cf.ByAutomationId("totalPrice")).AsLabel();
             Trace.Assert(totalPrice.Text == "55,00 kr");
-
-        }
-
-        [TestMethod]
-        public void TestAddProducts()
-        {
-            // Gets the elements for adding a new product button
-            TextBox nameTextBox = window.FindFirstDescendant(cf.ByAutomationId("itemNameTextBox")).AsTextBox();
-            TextBox priceTextBox = window.FindFirstDescendant(cf.ByAutomationId("itemPriceTextBox")).AsTextBox();
-            Button addProductButton = window.FindFirstDescendant(cf.ByName("Add Item")).AsButton();
-
-            // Adds a new product button
-            nameTextBox.Text = "TestProduct";
-            priceTextBox.Text = "1234";
-            addProductButton.Click();
-
-            // Checks that the new button exists and works
-            Button addedButton = window.FindFirstDescendant(cf.ByName("TestProduct")).AsButton();
-            addedButton.Click();
-            Label totalPrice = window.FindFirstDescendant(cf.ByAutomationId("totalPrice")).AsLabel();
-            Trace.Assert(totalPrice.Text == "1234,00 kr");
         }
     }
 }
