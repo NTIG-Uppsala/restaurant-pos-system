aa5c1ee81a3f88f4a9b0e1fd74eb3dfa8b661b9b
Tore-Forsberg <tore.forsberg@elev.ga.ntig.se>
2023-12-05 09:50:02 +0100

    Reformat and refactor MainWindow.xaml.cs
    Co-Authored-By: Fabian-Hemstrom <142995105+Fabian-Hemstrom@users.noreply.github.com>
    Co-Authored-By: Carl Lagerstedt <105862361+Carl595@users.noreply.github.com>

--- PointOfSaleSystem/PointOfSaleSystem/MainWindow.xaml.cs
+++ PointOfSaleSystem/PointOfSaleSystem/MainWindow.xaml.cs
@@ -1,25 +1,9 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.IO;
-using System.Reflection.Emit;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using MySql.Data.MySqlClient;
 
 
 namespace PointOfSaleSystem
@@ -38,8 +22,6 @@ namespace PointOfSaleSystem
         {
             InitializeComponent();
 
-            LoadDefault();
-
             // Load items from JSON file
             LoadItemsFromDatabase();
 
@@ -51,43 +33,11 @@ namespace PointOfSaleSystem
 
         }
 
-        public class data
-        {
-            public string Name { get; set; }
-            public double Price { get; set; }
-        }
-
-        private void LoadDefault()
-        {
-            List<data> _data = new List<data>();
-
-            _data.Add(new data()
-            {
-                Name = "Kaffe",
-                Price = 20.00
-            });
-
-            _data.Add(new data()
-            {
-                Name = "Bulle",
-                Price = 25.00
-            });
-
-            _data.Add(new data()
-            {
-                Name = "Kaka",
-                Price = 10.00
-            });
-
-            string json = System.Text.Json.JsonSerializer.Serialize(_data);
-            File.WriteAllText(@"items.json", json);
-        }
-
         private void InitializeFileSystemWatcher()
         {
             fileSystemWatcher = new FileSystemWatcher
             {
-                Path = Environment.CurrentDirectory, // Change this path if needed
+                Path = Environment.CurrentDirectory,
                 Filter = "items.json",
                 NotifyFilter = NotifyFilters.LastWrite
             };
@@ -111,30 +61,30 @@ namespace PointOfSaleSystem
         {
             try
             {
-                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                MySqlConnection connection = new MySqlConnection(ConnectionString);
+                connection.Open();
+
+                string query = "SELECT * FROM products";
+                MySqlCommand command = new MySqlCommand(query, connection);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                // Clear existing items
+                items.Clear();
+
+                while (reader.Read())
                 {
-                    connection.Open();
+                    int itemId = Convert.ToInt32(reader["ID"]);
+                    string? itemName = Convert.ToString(reader["Product"]);
+                    double itemPrice = Convert.ToDouble(reader["Price"]);
+                    int categoryId = Convert.ToInt32(reader["Category_ID"]);
 
-                    string query = "SELECT * FROM products";
-                    MySqlCommand command = new MySqlCommand(query, connection);
-
-                    using (MySqlDataReader reader = command.ExecuteReader())
-                    {
-                        // Clear existing items
-                        items.Clear();
-
-                        while (reader.Read())
-                        {
-                            int itemId = Convert.ToInt32(reader["ID"]);
-                            string itemName = reader["Product"].ToString();
-                            double itemPrice = Convert.ToDouble(reader["Price"]);
-                            int categoryId = Convert.ToInt32(reader["Category_ID"]);
-
-                            // Create an Item object and add it to the ObservableCollection
-                            items.Add(new Item(itemId, itemName, itemPrice, categoryId));
-                        }
-                    }
+                    // Create an Item object and add it to the ObservableCollection
+                    items.Add(new Item(itemId, itemName, itemPrice, categoryId));
                 }
+
+                reader.Dispose();
+                connection.Dispose();
             }
             catch (Exception ex)
             {
