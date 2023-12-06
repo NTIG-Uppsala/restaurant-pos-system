cfd454a25e7c9768c31000d0a6f59ec655c557dd
Tore-Forsberg <tore.forsberg@elev.ga.ntig.se>
2023-12-06 11:46:53 +0100

    Add a project for creating a SQLite database
    Co-Authored-By: Carl Lagerstedt <105862361+Carl595@users.noreply.github.com>
    Co-Authored-By: Fabian-Hemstrom <142995105+Fabian-Hemstrom@users.noreply.github.com>

---
+++ PointOfSaleSystem/DatabaseSetup/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+try
+{
+    using var db = new POSSContext();
+    db.Database.EnsureCreated();
+
+    // Note: This sample requires the database to be created before running.
+    Console.WriteLine($"Database path: {db.DbPath}.");
+
+    // Create
+    Console.WriteLine("Inserting a new prod");
+    db.Add(new Item { Name = "Kaffe", Price = 20, CategoryId = 4 });
+    db.SaveChanges();
+
+    // Read
+    Console.WriteLine("Querying for all products");
+    var products = db.Products;
+    foreach (var product in products)
+    {
+        Console.WriteLine($"Name: {product.Name}\nPrice: {product.Price}\nCategoryId: {product.CategoryId}");
+    }
+}
+catch (Exception error)
+{
+    Console.WriteLine($"An error occured during database setup: {error.Message}");
+}
