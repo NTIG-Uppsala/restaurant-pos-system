204a354411d8e078e14cfe9d59ed7627ca16a25a
Tore-Forsberg <tore.forsberg@elev.ga.ntig.se>
2024-01-22 09:35:29 +0100

    Fix tests that uses data from the product window

--- PointOfSaleSystem/TestSystem/TestSystem.cs
+++ PointOfSaleSystem/TestSystem/TestSystem.cs
@@ -380,10 +380,10 @@ namespace TestSystem
                 var itemNameHasBeenAdded = itemTable.Rows.Any(row => row.Cells[0].Value.ToString() == "Bearnaise");
                 Trace.Assert(itemNameHasBeenAdded);
 
-                var itemPriceHasBeenAdded = itemTable.Rows.Any(row => row.Cells[1].Value.ToString() == "10,00 kr" ^ row.Cells[1].Value.ToString() == "10.00 kr");
+                var itemPriceHasBeenAdded = itemTable.Rows.Any(row => row.Cells[2].Value.ToString() == "10,00 kr" ^ row.Cells[1].Value.ToString() == "10.00 kr");
                 Trace.Assert(itemPriceHasBeenAdded);
 
-                var itemAmountHasBeenAdded = itemTable.Rows.Any(row => row.Cells[2].Value.ToString() == "1");
+                var itemAmountHasBeenAdded = itemTable.Rows.Any(row => row.Cells[1].Value.ToString() == "1");
                 Trace.Assert(itemAmountHasBeenAdded);
             }
 
@@ -402,10 +402,10 @@ namespace TestSystem
                 var itemNameHasBeenAdded = itemTable.Rows.Any(row => row.Cells[0].Value.ToString() == "Bearnaise");
                 Trace.Assert(itemNameHasBeenAdded);
 
-                var itemPriceHasBeenAdded = itemTable.Rows.Any(row => row.Cells[1].Value.ToString() == "20,00 kr" ^ row.Cells[1].Value.ToString() == "20.00 kr");
+                var itemPriceHasBeenAdded = itemTable.Rows.Any(row => row.Cells[2].Value.ToString() == "20,00 kr" ^ row.Cells[1].Value.ToString() == "20.00 kr");
                 Trace.Assert(itemPriceHasBeenAdded);
 
-                var itemAmountHasBeenAdded = itemTable.Rows.Any(row => row.Cells[2].Value.ToString() == "2");
+                var itemAmountHasBeenAdded = itemTable.Rows.Any(row => row.Cells[1].Value.ToString() == "2");
                 Trace.Assert(itemAmountHasBeenAdded);
             }
 
